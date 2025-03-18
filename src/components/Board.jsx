ee89ef13ca9e0262941315d23be382bf88a8a04c
NitinTSutar <nitintsuthar67@gmail.com>
2025-03-19 01:12:33 +0530

    bug fix

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -9,7 +9,7 @@ export const Board = () => {
             [0, 3, 6],
             [1, 4, 7],
             [2, 5, 8],
-            [0, 2, 8],
+            [0, 4, 8],
             [2, 4, 6],
         ];
         const winner = lines
